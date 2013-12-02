14c5c3d756e0a2aab334923c6970f24b2cbddcad
Yeyande <kalebsma@gmail.com>
2013-12-02 16:59:08 -0600

    Updated StoryNode

--- Project/src/cs321game/StoryNode.java
+++ Project/src/cs321game/StoryNode.java
@@ -11,5 +11,22 @@ package cs321game;
  * @author yeyande
  */
 public class StoryNode {
+    public StoryNode m_Right;
+    public StoryNode m_Left;
+    private Scene m_Node;
     
+    public StoryNode(){
+        
+    }
+    
+    public void AddNode(StoryNode left, StoryNode right){
+        
+    }
+    
+    public void AddNode(StoryNode left){
+        this.AddNode(left, null);
+    }
+    public Scene getScene(){
+        return m_Node;
+    }
 }
